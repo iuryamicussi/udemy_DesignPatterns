6906bce090e257c69741991e440b76b51f3deaf4
iury <vapo_iury@hotmail.com>
2018-02-02 20:19:00 -0200

    adding decorator project.. not finished

---
+++ Udemy.DesignPatterns.Decorator/MultipleInheritance.cs
@@ -0,0 +1,16 @@
+﻿namespace Udemy.DesignPatterns.Decorator
+{
+    public class Bird
+    {
+        
+    }
+    
+    
+    public class MultipleInheritance
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
