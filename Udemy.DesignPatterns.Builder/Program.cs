35db5d5977cb1e4014cb4fe2d5f4d7696ba84b99
iury <vapo_iury@hotmail.com>
2018-02-05 19:44:37 -0200

    Fazendo os exercícios construindo os projetos..

---
+++ Udemy.DesignPatterns.Builder/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Text;
+
+namespace Udemy.DesignPatterns.Builder
+{
+    public class CodeBuilder
+    {
+        private StringBuilder sb = new StringBuilder();
+        private int indentation = 0;
+        
+        public CodeBuilder(string className)
+        {
+            WriteCode($"public class {className}",indentation);
+            WriteCode("{",indentation);
+        }
+        
+        public CodeBuilder AddField(string fieldName, string fieldType)
+        {
+            WriteCode($"public {fieldType} {fieldName};",indentation+1);
+            return this;
+        }
+        
+        public override string ToString()
+        {
+            return WriteCode("}",indentation).ToString();
+        }
+
+        private StringBuilder WriteCode(string content, int indentation)
+        {
+            return sb.AppendLine(new string(' ',indentation*2) + content);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cb = new CodeBuilder("Person").AddField("Name", "string").AddField("Age", "int");
+            Console.WriteLine(cb);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Udemy.DesignPatterns.Factory/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Udemy.DesignPatterns.Factory {
+
+    public class PersonFactory
+    {
+        private static int _id = 0;
+        public Person CreatePerson(string name)
+        {
+            return new Person{
+                Id = _id++,
+                Name = name
+            };
+        }
+    }
+
+    public class Person 
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public override string ToString()
+        {
+            return $"Id:{Id} Name:{Name}";
+        }
+
+    }
+
+    class Program
+    {
+        public static void Main(string []args)
+        {
+            var personFactory = new PersonFactory();
+            var john = personFactory.CreatePerson("John");
+            var maria = personFactory.CreatePerson("Maria");
+            System.Console.WriteLine(john);
+            System.Console.WriteLine(maria);
+        }
+    }
+}
