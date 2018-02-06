9836a696b0b04b85337d5687941b02345b2856b4
iury <vapo_iury@hotmail.com>
2018-02-05 23:36:20 -0200

    Prototype with object deepcopy example and assignment and Singleton with lazy instantiation

--- Udemy.DesignPatterns.Prototype/Program.cs
+++ Udemy.DesignPatterns.Prototype/Program.cs
@@ -1,12 +1,89 @@
 ﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
 
 namespace Udemy.DesignPatterns.Prototype
 {
+
+    public static class ExtensionMethos
+    {
+        public static T BinaryDeepCopy<T>(this T self)
+        {
+            var formatter = new BinaryFormatter();
+            using (var ms = new MemoryStream())
+            {
+                formatter.Serialize(ms,self);
+                ms.Seek(0, SeekOrigin.Begin);
+                return (T) formatter.Deserialize(ms);
+            }
+        }
+    }
+
+    [Serializable]
+    public class Point
+    {
+        public int X, Y;
+    }
+
+    [Serializable]
+    public class Line
+    {
+        public Point Start, End;
+
+        public Line DeepCopy()
+        {
+            return new Line
+            {
+                End = new Point
+                {
+                    X = this.End.X,
+                    Y = this.End.Y
+                },
+                Start = new Point
+                {
+                    X = this.Start.X,
+                    Y = this.Start.Y
+                }
+            };
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine($"Start Point = {{X}}:{Start.X}");
+            sb.AppendLine($"Start Point = {{Y}}:{Start.Y}");
+            sb.AppendLine();
+            sb.AppendLine($"End Point = {{X}}:{End.X}");
+            sb.AppendLine($"End Point = {{Y}}:{End.Y}");
+            return sb.ToString();
+        }
+    }
+    
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var line1 = new Line
+            {
+                End = new Point
+                {
+                    X = 1,
+                    Y = 2
+                },
+                Start = new Point
+                {
+                    X = 3,
+                    Y = 4
+                }
+            };
+            Console.WriteLine(line1);
+            var line2 = line1.DeepCopy();
+            line2.Start.X = 99;
+            var line3 = line2.BinaryDeepCopy();
+            Console.WriteLine(line2);
+            Console.WriteLine(line3);
+            Console.ReadKey();
         }
     }
 }
---
+++ Udemy.DesignPatterns.Singleton/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Udemy.DesignPatterns.Singleton
+{
+    public class SingletonTester
+    {
+        public static bool IsSingleton(Func<object> func)
+        {
+            var instance1 = func.Invoke();
+            var instance2 = func.Invoke();
+
+            return instance1.Equals(instance2);
+        }
+    }
+    
+    public class Singleton
+    {
+        private  static  Lazy<Singleton> _s = new Lazy<Singleton>(()=> new Singleton());
+
+        public static Singleton ReturnSingleton() => _s.Value;
+    }
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Is {SingletonTester.IsSingleton(Singleton.ReturnSingleton)} that Singleton is a singleton!");
+            Console.ReadKey();
+        }
+    }
+}
